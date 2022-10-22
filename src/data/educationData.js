d5ada4e05b4ddb8cde542e2b40c4477f9e5730bd
MShahabuddin <mshahabuddin@jazeeratech.in>
2022-10-23 02:43:33 +0530

    changes

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
-    {
-        id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
-    },
-    {
-        id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
-    },
-    {
-        id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
-    },
-]
+  {
+    id: 2,
+    institution: "R D Inter Collage Gorakhpur",
+    course: "Senior Secondary Education",
+    startYear: "2013",
+    endYear: "2015",
+  },
+  {
+    id: 2,
+    institution: "Buddha Institue of Technology",
+    course: "Bachelor of Technology",
+    startYear: "2016",
+    endYear: "2020",
+  },
+  // {
+  //     id: 3,
+  //     institution: 'Hawking University, Cambridge',
+  //     course: 'Master of Technology',
+  //     startYear: '2019',
+  //     endYear: 'Present'
+  // },
+];
