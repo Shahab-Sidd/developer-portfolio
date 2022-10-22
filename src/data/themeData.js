ab4fea3e1dc52625f6359b1945d2ae672b0acb27
MShahabuddin <mshahabuddin@jazeeratech.in>
2022-10-23 04:26:38 +0530

    Revert "changes"
    This reverts commit 79c2458e9ee416d3926041328f412128ff9c93e0.

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,25 +1,12 @@
 /* eslint-disable */
-import {
-  greenThemeLight,
-  greenThemeDark,
-  bwThemeLight,
-  bwThemeDark,
-  blueThemeLight,
-  blueThemeDark,
-  redThemeLight,
-  redThemeDark,
-  orangeThemeLight,
-  orangeThemeDark,
-  purpleThemeLight,
-  purpleThemeDark,
-  pinkThemeLight,
-  pinkThemeDark,
-  yellowThemeLight,
-  yellowThemeDark,
-} from "../theme/theme";
+import { 
+    greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
+} from '../theme/theme'
+
 
 export const themeData = {
-  theme: bwThemeLight,
-};
+    theme: orangeThemeDark
+}
+
 
 // Choose theme from above
