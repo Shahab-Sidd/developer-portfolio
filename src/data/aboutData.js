605b03e8d15c29fef04ef93794ecd0bda91fbd90
MShahabuddin <mshahabuddin@jazeeratech.in>
2022-11-01 19:50:51 +0530

    changes okk

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name's Mohd Shahabuddin. I'm a web developer based in Hyderabad, India.",
+    "My name is Mohd Shahabuddin. I'm a web developer based in Hyderabad, India.",
   description2: "Description will come here",
   image: 2,
 };
